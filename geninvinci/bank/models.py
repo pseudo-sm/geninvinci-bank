5e02393d1e979d483956b90650fe21c16c710ea7
pseudo-sm <saswathcommand@gmail.com>
2023-03-03 11:55:51 +0530

    added signin view

--- geninvinci/bank/models.py
+++ geninvinci/bank/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Account(models.Model):
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+    name = models.CharField(max_length=255)
+    account_no = models.BigIntegerField(primary_key=True)
+
+class Transaction(models.Model):
+
+    transaction_id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(Account,on_delete=models.CASCADE,related_name="sender_account")
+    receiver = models.ForeignKey(Account,on_delete=models.CASCADE,related_name="receiver_account")
+    amount = models.FloatField()
+    datetime = models.DateTimeField(auto_created=True)
+
+
+
